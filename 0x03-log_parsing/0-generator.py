5eeed06ca43c67e464513957d6599af01a7bd1e9
bonnett93 <bonnettjordan@gmail.com>
2022-05-06 11:01:32 -0500

    0-stats: Delete check_format func, add try except. 0-generator: Add bad format and bad status codes

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -6,10 +6,23 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
+    # Rigth format
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
         datetime.datetime.now(),
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    # Bad format
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\"\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now()
+    ))
+    # Bad status code
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\"\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([201, 303, 409, 410, 411, 414, 415, 555]),
         random.randint(1, 1024)
     ))
     sys.stdout.flush()
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,31 +5,6 @@
 import sys
 from datetime import datetime
 
-
-def check_format(line_split):
-    """
-    Check the correct format of the line readed
-    """
-    try:
-        ip = line_split[0].split('.')
-        if not all(map(str.isdigit, ip)):
-            print("PAILAS IP")
-            return False
-
-        date_str = line_split[2][1:]+' '+line_split[3][:-1]
-        date_format = "%Y-%m-%d %H:%M:%S.%f"
-        datetime.strptime(date_str, date_format)
-
-        if line_split[-2] not in ["200", "301", "400", "401",
-                                  "403", "404", "405", "500"]:
-            return False
-
-        int(line_split[-1])
-        return True
-    except:
-        return False
-
-
 def printstats(file_size, status_codes):
     """
     print stats about file_size and status_codes
@@ -45,11 +20,10 @@ file_size = 0
 status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
-try:
-    for line in sys.stdin:
+
+for line in sys.stdin:
+    try:
         line_split = line.split()
-        if not check_format(line_split):
-            continue
         file_size += int(line_split[-1])
         status_code = int(line_split[-2])
         status_dict[status_code] += 1
@@ -59,5 +33,7 @@ try:
             x = 0
             printstats(file_size, status_codes)
 
-except KeyboardInterrupt:
-    printstats(file_size, status_codes)
+    except KeyboardInterrupt:
+        printstats(file_size, status_codes)
+    except Exception:
+        continue
